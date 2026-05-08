7e75654706426921580677fa39419838e71a7e96
Krzysztof Probola <32790662+rozmiarD@users.noreply.github.com>
2026-05-09 00:03:18 +0200

    release: calibrate GovEngine package description

--- govengine/__init__.py
+++ govengine/__init__.py
@@ -5,7 +5,7 @@ contracts without importing optional contract-lifecycle dependencies at package
 import time.
 """
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from .api import GovApiError, GovApiResult
 from .context import GovEngineContext, GovEnginePaths, ravenclaw_context
